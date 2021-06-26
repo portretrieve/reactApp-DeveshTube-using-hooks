468480de584db8a441b48c755a1fc527a49689f3
Devesh Kumar <deveshkumar2107@hotmail.com>
2021-06-26 16:23:59 -0400

    updated SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,37 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchBar.css";
-class SearchBar extends React.Component {
-  state = { userInput: "" };
+const SearchBar = ({ onSearch }) => {
+  const [userInput, setUserInput] = useState("");
 
-  onInputChange = (event) => {
-    this.setState({ userInput: event.target.value });
+  const onInputChange = (event) => {
+    setUserInput(event.target.value);
   };
 
-  onFormSubmit = (event) => {
+  const onFormSubmit = (event) => {
     event.preventDefault();
-    this.props.onSearch(this.state.userInput);
+    onSearch(userInput);
   };
 
-  render() {
-    return (
-      <div className="search-bar ui segment">
-        <form className="ui form" onSubmit={this.onFormSubmit}>
-          <div className="field">
-            <input
-              type="text"
-              name="videoSearch"
-              id="videoSearch"
-              value={this.state.userInput}
-              onChange={this.onInputChange}
-              placeholder="Type to search video"
-            />
-            <br /> <br />
-            <button>Click to Search</button>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search-bar ui segment">
+      <form className="ui form" onSubmit={onFormSubmit}>
+        <div className="field">
+          <input
+            type="text"
+            name="videoSearch"
+            id="videoSearch"
+            value={userInput}
+            onChange={onInputChange}
+            placeholder="Type to search video"
+          />
+          <br /> <br />
+          <button>Click to Search</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
